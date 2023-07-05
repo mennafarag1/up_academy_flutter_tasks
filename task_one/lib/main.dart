3758a2aee1c7cb06eec1e20e6ec9afb9436cc135
menna farag <menna.a.farag2000@gmail.com>
2023-07-06 02:33:07 +0300

    task ui

--- task_one/lib/main.dart
+++ task_one/lib/main.dart
@@ -1,29 +1,108 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const RootWidget());
+  runApp(const BusinessCardApp());
 }
 
-class RootWidget extends StatelessWidget {
-  const RootWidget({super.key});
+class BusinessCardApp extends StatelessWidget {
+  const BusinessCardApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return  MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Up Academy',
-      home: SafeArea(
-        child: Scaffold(
-          appBar: AppBar(
-            backgroundColor: Colors.black,
-            title: Text('menna'),
-            centerTitle: true,
-          ),
-          body: Center(
-              child: Text(
-            'Up Academy',
-            style: TextStyle(fontSize: 35, color: Colors.black),
-          )),
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Color(0xffccae6b),
+          title: Text('busniess card'),
+          centerTitle: true,
+
+        ),
+        backgroundColor: Color(0xffccae6b),
+        body: Column(
+          children: [
+            CircleAvatar(
+              radius: 122,
+              backgroundColor: Colors.white,
+              child: CircleAvatar(
+                radius: 120,
+                backgroundImage: AssetImage('images/menna.jpeg'),
+              ),
+            ),
+            Text(
+              'Menna Ahmed',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 32,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+             Text(
+              'Software Engineer',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 32,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(
+              height: 40,
+            ),
+            Container(
+              color: Colors.white,
+              child: Row(
+                children: [
+                    SizedBox(
+                    width: 20,
+                  ),
+                  Icon(
+                    Icons.phone,
+                    size: 40,
+                    color: Color(0xFF2B475E),
+                  ),
+                  Spacer(),
+                  Text(
+                    '01100812422',
+                    style: TextStyle(fontSize: 24),
+                  ),
+                  Spacer(
+                    flex: 2,
+                  )
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+             Container(
+                
+              color: Colors.white,
+              child: Row(
+                children: [
+                   SizedBox(
+                    width: 20,
+                  ),
+                  Icon(
+                    Icons.mail,
+                    size: 40,
+                    color: Color(0xFF2B475E),
+                  ),
+                  Spacer(),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Text(
+                   
+                    'menna.a.farag@ gmail.com',
+                    style: TextStyle(fontSize: 24),
+                  ),
+                  Spacer(
+                    flex: 2,
+                  )
+                ],
+              ),
+            ),
+          ],
         ),
       ),
     );
